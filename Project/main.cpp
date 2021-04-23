7b0ecafe44f836f1fc1bc586498b5b7167a438ca
liaoyilan <2425494170@qq.com>
2021-04-23 14:59:54 +0800

    Initial Commit

---
+++ Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project
+//
+//  Created by Lyilan on 2021/4/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
